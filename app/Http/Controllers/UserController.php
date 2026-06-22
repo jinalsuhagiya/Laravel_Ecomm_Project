bb495880b687e1db3ebd73173da3aed0ebc337b1
Jinal <81014173+jinalsuhagiya@users.noreply.github.com>
2026-06-22 13:52:38 +0530

    SMTP Mail Notification

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,11 +5,20 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Notifications\WelcomeNotification;
+use Illuminate\Support\Facades\Notification;
 
 class UserController extends Controller
 {
 public function index()
 {
-    return view('dashboard');
+   $users = User::whereNotNull('email')->get();
+
+    Notification::send($users, new WelcomeNotification());
+
+    return "All Users Mail Sent Successfully";
+
+//    return view('dashboard');
 }
 }
